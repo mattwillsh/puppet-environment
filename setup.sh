511c79efd7114e52c05c9a34cccef17c6d66941d
Matt Willsher <matt@monki.org.uk>
2012-08-26 23:00:46 +0100

    Add LXC CentOS template

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # You may also want https://github.com/rodjek/vim-puppet 
 # There a packages for it in Ubuntu if you wish
 
+
 set -e
 
 tmpdir='/tmp/puppetinstall'
@@ -16,6 +17,11 @@ if [[ ! -f $pupfile ]]; then
   wget http://apt.puppetlabs.com/$pupfile
 fi
 sudo dpkg -i $pupfile
-sudo apt-get install puppet rubygems lxc rubygems
+sudo apt-get install puppet rubygems  
 sudo gem install puppetlabs_spec_helper
+
+
+# Set up LXC for CentOS
+sudo apt-get install yum lxc 
+sudo cp lxc-centos /usr/lib/lxc/templates
 
